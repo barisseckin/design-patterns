096314385e4cb5d8ff9a5e95616412270ae964f0
Baris0 <barisseckin0@gmail.com>
2022-10-16 22:11:12 +0300

    builder pattern

--- src/main/java/builder/Main.java
+++ src/main/java/builder/Main.java
@@ -1,12 +1,23 @@
 package builder;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
         Product product = new Product();
         product.setDescription("test description");
         product.setName("test name");
 
-        
+        Product product2 = new Product.Builder()
+                .id("2")
+                .name("test")
+                .createDate(LocalDateTime.now())
+                .description("test")
+                .inStock(true)
+                .amount(BigDecimal.valueOf(10000))
+                .build();
 
+        System.out.println(product2);
     }
 }
